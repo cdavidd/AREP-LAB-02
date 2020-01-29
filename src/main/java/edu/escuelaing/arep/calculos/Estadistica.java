65b010ef743b13bffdd1d10a7f79e56c60d3ecee
david <cdavidlopez@hotmail.com>
2020-01-29 18:17:14 -0500

    fin

---
+++ src/main/java/edu/escuelaing/arep/calculos/Estadistica.java
@@ -0,0 +1,38 @@
+package edu.escuelaing.arep.calculos;
+
+import java.util.List;
+
+public class Estadistica {
+
+    /**
+     * Metodo que calcula la media de los datos ingresados
+     * 
+     * @param datos
+     * @return double
+     */
+    public static double mean(List<Double> datos) {
+        double cont = 0;
+        for (double n : datos) {
+            cont += n;
+        }
+        double promedio = cont /= datos.size();
+        return promedio;
+    }
+
+    /**
+     * Metodo que calcula la desviacion estandar de los datos ingresados
+     * 
+     * @param datos
+     * @return double
+     */
+    public static double stdDev(List<Double> datos) {
+        int size = datos.size();
+        double cont = 0;
+
+        for (double n : datos) {
+            cont += Math.pow((n - mean(datos)), 2);
+        }
+        cont /= (size - 1);
+        return Math.sqrt(cont);
+    }
+}
--- src/main/java/edu/escuelaing/arep/designprimer/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/designprimer/SparkWebApp.java
@@ -6,6 +6,12 @@
 package edu.escuelaing.arep.designprimer;
 
 import static spark.Spark.*;
+
+import edu.escuelaing.arep.calculos.Estadistica;
+import edu.escuelaing.arep.model.LinkedList;
+import spark.Request;
+import spark.Response;
+
 /**
  *
  * @author cristian.lopez-a
@@ -13,12 +19,47 @@ import static spark.Spark.*;
 public class SparkWebApp {
     public static void main(String[] args) {
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");    
+        get("/", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
     }
+
+    public static String inputDataPage(Request req, Response res) {
+        String pageContent = "<!DOCTYPE html>" + "<html>" + "<body>" + "<h1>Calcula la desviacion estandar y media</h1>"
+                + "<p>Ingresar los numeros separados por coma ',' Ej: 1,2,3</p>" + "<form action=\"/results\">"
+                + "  Datos a ingresar:<br>" + "  <input type=\"text\" name=\"datos\" placeholder=\"1,2,3\">" + "  <br>"
+                + "  <input type=\"submit\" value=\"Calcular\">" + "</form>" + "</body>" + "</html>";
+        return pageContent;
+    }
+
+    public static String resultsPage(Request req, Response res) {
+        String datos = req.queryParams("datos");
+        String[] datoS = datos.split(",");
+        LinkedList<Double> datosD = new LinkedList<>();
+        for (String s : datoS) {
+            datosD.add(Double.parseDouble(s));
+        }
+        String mean = String.valueOf(calcularMean(datosD));
+        String std = String.valueOf(calcularstdDev(datosD));
+
+        String pagina = "<!DOCTYPE html>" + "<html>" + "<body>" + "<h1>Resultados</h1>\n" + "La media es: " + mean
+                + "<br>" + "La desviacion estandar es: " + std + "<br>" + "<a href=\"/\">Volver</a>" + "</body>"
+                + "</html>";
+
+        return pagina;
+    }
+
+    public static Double calcularMean(LinkedList<Double> datos) {
+        return Estadistica.mean(datos);
+    }
+
+    public static Double calcularstdDev(LinkedList<Double> datos) {
+        return Estadistica.stdDev(datos);
+    }
+
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
-        }        
-    return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)    
+        }
+        return 4567; // returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
