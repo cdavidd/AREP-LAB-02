adc96ec0aa610fdf88baf273ab8139de9ee8bf85
david <cdavidlopez@hotmail.com>
2020-01-30 10:30:53 -0500

    test

--- src/test/java/edu/escuelaing/arep/AppTest.java
+++ src/test/java/edu/escuelaing/arep/AppTest.java
@@ -1,38 +1,56 @@
 package edu.escuelaing.arep;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import java.text.DecimalFormat;
+
+import edu.escuelaing.arep.calculos.Estadistica;
+import edu.escuelaing.arep.model.LinkedList;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    @Test
+    public void testMean() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        LinkedList<Double> datos = new LinkedList<Double>();
+        datos.add(15.0);
+        datos.add(69.9);
+        datos.add(6.5);
+        datos.add(22.4);
+        datos.add(28.4);
+        datos.add(65.9);
+        datos.add(19.4);
+        datos.add(198.7);
+        datos.add(38.8);
+        datos.add(138.2);
+        double mean = Estadistica.mean(datos);
+        String res = df.format(mean);
+        double r = 60.32;
+        String res1 = df.format(r);
+        assertEquals(res1, res);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testStdDev() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        LinkedList<Double> datos = new LinkedList<Double>();
+        datos.add(160d);
+        datos.add(591d);
+        datos.add(114d);
+        datos.add(229d);
+        datos.add(230d);
+        datos.add(270d);
+        datos.add(128d);
+        datos.add(1657d);
+        datos.add(624d);
+        datos.add(1503d);
+        double std = Estadistica.stdDev(datos);
+        String res = df.format(std);
+        double r = 572.03;
+        String res1 = df.format(r);
+        assertEquals(res1, res);
     }
 }
